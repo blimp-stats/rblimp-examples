1d77c23190df06c7237a9e56a8ab2963ac1e6a3a
craigenders <cenders@ucla.edu>
2025-04-19 14:32:19 -0700

    Updates

--- 3 Convergence Diagnostics/Ex3a.R
+++ 3 Convergence Diagnostics/Ex3a.R
@@ -4,17 +4,18 @@ connect <- url('https://raw.githubusercontent.com/blimp-stats/rblimp-examples/ma
 data <- readRDS(connect); close(connect)
 
 mymodel <- rblimp(
-   data = data,
-   clusterid = 'level2id',
-   ordinal = 'd1.j',
-   fixed = 'd1.j',
-   center = 'groupmean = x1.i;
-   grandmean = x2.i x7.j d1.j',
-   model = ' y.i ~ x1.i x2.i x7.j d1.j | x1.i',
-   seed = 90291,
-   burn = 10000,
-   iter = 10000,
-   options = 'labels')
+  data = data,
+  clusterid = 'level2id',
+  ordinal = 'd1_j',
+  fixed = 'd1_j',
+  center = '
+   groupmean = x1_i;
+   grandmean = x2_i x3_j d1_j',
+  model = ' y_i ~ x1_i x2_i x3_j d1_j | x1_i',
+  seed = 90291,
+  burn = 10000,
+  iter = 10000,
+  options = 'labels')
 
 output(mymodel)
 
