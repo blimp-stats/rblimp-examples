42f90141c6bd57530f0fb8c877cbb0928e3c7c86
craigenders <cenders@ucla.edu>
2025-04-19 15:55:40 -0700

    Updates

--- Ch 4 Regression Models/Ex4.13. Logistic Regression With a Binary Outcome.R
+++ Ch 4 Regression Models/Ex4.13. Logistic Regression With a Binary Outcome.R
@@ -25,6 +25,7 @@ mymodel <- rblimp(
   center = 'x1 x2',
   model = 'logit(y) ~ 1@b0 x1 x2 d@b3',
   parameters = '
+  # conditional predicted probabilities
    pp_d0 = exp(b0) / (1 + exp(b0));
    pp_d1 = exp(b0 + b3) / (1 + exp(b0 + b3));
    pp_diff = pp_d1 - pp_d0',
