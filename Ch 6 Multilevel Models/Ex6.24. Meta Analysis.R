305a3711ba3aa625cc8d822c4e86555684380f23
craigenders <cenders@ucla.edu>
2025-12-13 17:09:58 -0800

    Updates

--- Ch 6 Multilevel Models/Ex6.24. Meta Analysis.R
+++ Ch 6 Multilevel Models/Ex6.24. Meta Analysis.R
@@ -1,38 +1,37 @@
 library(rblimp)
-library(metafor)
 
-# effect size data from metafor
-data <- dat.raudenbush1985
+connect <- url('https://raw.githubusercontent.com/blimp-stats/rblimp-examples/main/Data/Ex6.24.RDS', 'rb')
+data <- readRDS(connect); close(connect)
 
 # random effects meta analysis
 mymodel <- rblimp(
   data = data,
-  transform = 'log_vi = ln(vi)',
+  transform = 'log_varj = ln(var_j)',
   clusterid = 'study',
-  latent = 'study = alpha_i',
+  latent = 'study = beta0_j',
   model = '
-    alpha_i ~ intercept;
-    yi ~ intercept@alpha_i;
-    var(yi) ~ 1@0 # fix variance of each study
-      (study==1)*log_vi@1
-      (study==2)*log_vi@1 
-      (study==3)*log_vi@1 
-      (study==4)*log_vi@1
-      (study==5)*log_vi@1
-      (study==6)*log_vi@1
-      (study==7)*log_vi@1
-      (study==8)*log_vi@1
-      (study==9)*log_vi@1
-      (study==10)*log_vi@1
-      (study==11)*log_vi@1
-      (study==12)*log_vi@1
-      (study==13)*log_vi@1
-      (study==14)*log_vi@1
-      (study==15)*log_vi@1
-      (study==16)*log_vi@1
-      (study==17)*log_vi@1
-      (study==18)*log_vi@1
-      (study==19)*log_vi@1;',
+    beta0_j ~ intercept;
+    es_j ~ intercept@beta0_j;
+    var(es_j) ~ 1@0 # fix variance of each study
+      (study==1)*log_varj@1
+      (study==2)*log_varj@1 
+      (study==3)*log_varj@1 
+      (study==4)*log_varj@1
+      (study==5)*log_varj@1
+      (study==6)*log_varj@1
+      (study==7)*log_varj@1
+      (study==8)*log_varj@1
+      (study==9)*log_varj@1
+      (study==10)*log_varj@1
+      (study==11)*log_varj@1
+      (study==12)*log_varj@1
+      (study==13)*log_varj@1
+      (study==14)*log_varj@1
+      (study==15)*log_varj@1
+      (study==16)*log_varj@1
+      (study==17)*log_varj@1
+      (study==18)*log_varj@1
+      (study==19)*log_varj@1;',
   seed = 90291,
   burn = 10000,
   iter = 10000
