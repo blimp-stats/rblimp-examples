20ee63a5c2253d04e22051621bda17cf68b378e7
craigenders <cenders@ucla.edu>
2025-12-15 14:35:30 -0800

    Updates

--- Ch 4 Regression Models/Ex4.19. Skewed Predictor and Yeo-Johnson Transform.R
+++ Ch 4 Regression Models/Ex4.19. Skewed Predictor and Yeo-Johnson Transform.R
@@ -11,7 +11,7 @@ mymodel <- rblimp(
    focal.model:
    logit(y) ~ x1 x2 d1 d2;
    predictor.model:
-   yjt(x2 - 16) ~ x1 d1; 
+   yjt(x2 - 15.5) ~ x1 d1; 
    d2 ~ x2 x1 d1',
    seed = 90291,
    burn = 1000,
