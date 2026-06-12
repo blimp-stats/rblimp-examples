662f814639db6f5335ccde15d3612267ecda98ee
craigenders <cenders@ucla.edu>
2026-06-11 18:46:53 -0700

    Updates

--- Ch 6 Multilevel Models/Ex6.24. Residual Dynamic SEM.R
+++ Ch 6 Multilevel Models/Ex6.24. Residual Dynamic SEM.R
@@ -8,19 +8,21 @@ mymodel <- rblimp(
   data = data,
   clusterid = 'level2id',
   timeid = 'time',
-  latent = 'level2id = ymean_j xmean_j phi_j beta_j trend_j',
+  latent = 'level2id = ymean_j xmean_j phi_j beta_1j beta_2j',
   model = '
-    # definition variable for the predicted value of y_i.lag
-    y_i_lag_hat = ymean_j + (time-1)*trend_j + x_i.lag*beta_j; 
-    # remove default dsem prior on missing time = 0 outcome
-    y_i_lag_res = ifelse(time <= 1, 0, y_i.lag - y_i_lag_hat);
-    level2.models:
-    1 -> ymean_j xmean_j phi_j beta_j trend_j;
-    level1.models:
-    x_i ~ 1@xmean_j;
-    y_i ~ 1@ymean_j y_i_lag_res@phi_j x_i@beta_j time@trend_j;',
+   # definition variable for the predicted value of y_i.lag
+   lag_yhat = ymean_j + (time - 1)*beta_2j + (x_i.lag - xmean_j)*beta_1j;
+   # definition variable for residual of y_i.lag, removing default dsem prior at t = 0
+   lag_yres = ifelse(time <= 1, 0, y_i.lag - lag_yhat);
+   level2.models:
+   1 -> ymean_j xmean_j phi_j beta_1j beta_2j;
+   level1.models:
+   x_i ~ 1@xmean_j;
+   y_i ~ 1@ymean_j lag_yres@phi_j (x_i - xmean_j)@beta_1j time@beta_2j;',
   seed = 90291,
   burn = 10000,
-  iter = 10000)
+  iter = 10000
+)
 
 output(mymodel)
+posterior_plot(mymodel)
