42627e1cb3ef7ad007e7e47cf93d18c3faa4e12a
Brian Keller <bk@bkeller2.com>
2026-07-01 12:46:18 -0500

    Update with mediated effect prior

--- Ch 5 Path Analysis and Latent Variable Models/Ex5.1. Mediation Analysis.R
+++ Ch 5 Path Analysis and Latent Variable Models/Ex5.1. Mediation Analysis.R
@@ -16,3 +16,21 @@ mymodel <- rblimp(
 output(mymodel)
 posterior_plot(mymodel,'indirect')
 posterior_plot(mymodel)
+
+
+# Specify with prior on indirect effect
+mymodelb <- rblimp(
+  data = data,
+  model = '
+   m ~ x@alpha%1;
+   y ~ x (m / alpha)@indirect;', 
+  parameters = '
+  indirect ~ normal(0, 10); # Informative Prior on Indirect effect
+  alpha ~ normal(0, Inf);   # Uniformative Prior on A path',
+  seed = 90291,
+  burn = 20000,
+  iter = 20000)
+
+output(mymodelb)
+posterior_plot(mymodelb,'indirect')
+posterior_plot(mymodelb)
